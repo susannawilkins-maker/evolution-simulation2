3b45534ddc2f62c3b1e6bd49332d0b6d5f334294
susannawilkins-maker <susanna.wilkins@cng.edu>
2026-04-07 14:46:25 -0500

    Update evolution_bot.py

--- evolution_bot.py
+++ evolution_bot.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from evolution_bot import EvolutionSimulationBot
 
 st.set_page_config(page_title="Evolution Simulation", layout="wide")
 st.title("Classroom Evolution Simulation")
