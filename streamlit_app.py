12bdbf12e4586c23263501d1fe00efb8ab2db97d
susannawilkins-maker <susanna.wilkins@cng.edu>
2026-04-07 14:41:14 -0500

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from evolution_bot import EvolutionSimulationBot
-
-st.set_page_config(page_title="🧬 Evolution Simulation", layout="wide")
-
-st.title("🧬 Classroom Evolution Simulation")
-st.markdown("**Simulate natural selection, mutations
